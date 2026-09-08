a9987de1477644d19ec4f6a02fa6cc8def7cc8e2
m-salman-afzal <53542416+m-salman-afzal@users.noreply.github.com>
2026-09-09 00:23:21 +0500

    updated config

--- zsh/10-env.zsh
+++ zsh/10-env.zsh
@@ -11,10 +11,10 @@ export VISUAL='nvim'
 export PATH=$HOME/.cargo/bin:$PATH
 
 #* bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH=$BUN_INSTALL/bin:$PATH
-# bun completions
-[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+# export BUN_INSTALL="$HOME/.bun"
+# export PATH=$BUN_INSTALL/bin:$PATH
+# # bun completions
+# [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
 #* pnpm
 export PNPM_HOME="$HOME/.local/share/pnpm"
